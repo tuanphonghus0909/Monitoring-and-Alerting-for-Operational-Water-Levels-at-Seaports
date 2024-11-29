0f87617008f39a8a4ff0fc984dda1a0575b5885c
Tran Tuan Phong <trantuanphong09092003@gmail.com>
2024-11-29 09:06:09 +0700

    hide secrets

--- src/secrets/wifi.h
+++
@@ -1,9 +0,0 @@
-#include <cstdint>
-namespace WiFiSecrets
-{
-    const char *ssid = "P404";
-    const char *pass = "66668888404";
-    unsigned int publish_count = 0;
-    uint16_t keepAlive = 15;
-    uint16_t socketTimeout = 5;
-}
